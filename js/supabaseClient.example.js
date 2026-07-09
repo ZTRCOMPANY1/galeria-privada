f94938b975e09e67eb31186c9631b446e837ec25
ZTR COMPANY <ztrcompany1@gmail.com>
2026-07-09 17:28:35 -0300

    asasa

--- js/supabaseClient.example.js
+++ js/supabaseClient.example.js
@@ -11,7 +11,7 @@
 //    (Project Settings > API)
 // ============================================================
 
-const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
-const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
+const SUPABASE_URL = "https://nhdxcvxdmxgurrpafglj.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oZHhjdnhkbXhndXJycGFmZ2xqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MTY1MzksImV4cCI6MjA5OTE5MjUzOX0.56PtsdYfiQ8qWMtjaPKwdK14xLei0JnTmIsNgUDKKTk";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
